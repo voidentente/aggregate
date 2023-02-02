c72da1128b5ea082c280cb049040bf21115e9907
Voidentente <flood.void@gmail.com>
2023-02-02 03:01:07 +0100

    Clean up `Amalgamate::fmt`

--- aggregate_types/src/fmt.rs
+++ aggregate_types/src/fmt.rs
@@ -1,40 +1,40 @@
-use crate::Amalgamate;
+use crate::{Amalgamate, Attributes, Fields};
 use quote::ToTokens;
-use std::fmt;
-use std::fmt::Formatter;
 
-impl fmt::Debug for Amalgamate {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+impl std::fmt::Debug for Amalgamate {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut buf = String::new();
-        debug(&mut buf, self, 0);
-        write!(f, "{buf}")
+        debug_amalgamate(&mut buf, self, 0);
+        f.write_str(&buf)
     }
 }
 
-fn debug(buf: &mut String, amalgamate: &Amalgamate, mut indentation: u8) {
-    const INDENTATION_GROWTH: u8 = 4;
+const INDENTATION_GROWTH: u8 = 4;
 
-    for (_, attr) in &amalgamate.attrs {
-        let attr = attr.to_token_stream().to_string();
-        indent(buf, indentation);
-        buf.push_str(&format!("{attr}\n"));
-    }
-
+fn debug_amalgamate(buf: &mut String, amalgamate: &Amalgamate, indentation: u8) {
+    debug_attrs(buf, &amalgamate.attrs, indentation);
     indent(buf, indentation);
     buf.push_str("amalgamate:\n");
-    indentation += INDENTATION_GROWTH;
+    debug_fields(buf, &amalgamate.fields, indentation + INDENTATION_GROWTH);
+}
 
-    for (ident, field) in &amalgamate.fields {
-        for (_, attr) in &field.attrs {
-            let attr = attr.to_token_stream().to_string();
-            indent(buf, indentation);
-            buf.push_str(&format!("{attr}\n"));
-        }
+fn debug_fields(buf: &mut String, fields: &Fields, indentation: u8) {
+    for (ident, field) in fields {
+        debug_attrs(buf, &field.attrs, indentation);
         indent(buf, indentation);
-        buf.push_str(&format!("{ident}:\n"));
-        if let Some(inner) = field.inner {
-            debug(buf, inner, indentation + INDENTATION_GROWTH);
+        buf.push_str(ident);
+        buf.push_str(":\n");
+        if let Some(amalgamate) = field.inner {
+            debug_amalgamate(buf, amalgamate, indentation + INDENTATION_GROWTH);
         }
+    }
+}
+
+fn debug_attrs(buf: &mut String, attrs: &Attributes, indentation: u8) {
+    for (_, attr) in attrs {
+        indent(buf, indentation);
+        buf.push_str(&attr.to_token_stream().to_string());
+        buf.push('\n');
     }
 }
 
